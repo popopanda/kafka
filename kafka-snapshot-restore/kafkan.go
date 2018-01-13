62ad798c11c0a53a78af7320936a884d156e6daf
popopanda <leung.jeff1120@gmail.com>
2018-01-12 19:52:45 -0800

    next steps

--- kafka-snapshot-restore/kafkan.go
+++ kafka-snapshot-restore/kafkan.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -10,19 +11,24 @@ import (
 )
 
 func main() {
-	env := ""
-	brokers := []string{"kafka-mirror-3", "kafka-mirror-4", "kafka-mirror-5"}
+	env := "uat2"
+	// brokers := []string{"kafka-mirror-3", "kafka-mirror-4", "kafka-mirror-5"}
+	brokers := map[string]string{
+		"kafka-mirror-0": "us-east-1b",
+		"kafka-mirror-1": "us-east-1d",
+		"kafka-mirror-2": "us-east-1e",
+	}
 	mountPoints := []string{"/dev/sds", "/dev/sdt", "/dev/sdu"}
 
 	sess := session.Must(session.NewSession())
 	svc := ec2.New(sess)
 
+	getNewKafkaVolumes(svc, env)
 	getSnapshot(svc, env, brokers, mountPoints)
-	// getNewKafkaVolumes(svc, env)
 
 }
 
-func getSnapshot(svc *ec2.EC2, environment string, brokers, mountPoints []string) {
+func getSnapshot(svc *ec2.EC2, environment string, brokers map[string]string, mountPoints []string) {
 	result, err := svc.DescribeSnapshots(&ec2.DescribeSnapshotsInput{
 		Filters: []*ec2.Filter{
 			&ec2.Filter{
@@ -42,16 +48,18 @@ func getSnapshot(svc *ec2.EC2, environment string, brokers, mountPoints []string
 		for _, tag := range snapshot.Tags {
 			if *tag.Key == *aws.String("Restore") && *tag.Value == *aws.String("true") {
 				for _, definedTags := range snapshot.Tags {
-					for _, broker := range brokers {
+					for broker, az := range brokers {
 						if *definedTags.Key == *aws.String("Broker") && *definedTags.Value == *aws.String(broker) {
 							for _, mountTag := range snapshot.Tags {
 								for _, mount := range mountPoints {
 									if *mountTag.Key == *aws.String("Mount") && *mountTag.Value == *aws.String(mount) {
-										fmt.Println()
-										fmt.Printf("%v: %v \n", *tag.Key, *tag.Value)
-										fmt.Printf("%v: %v === %v \n", *definedTags.Key, *definedTags.Value, broker)
-										fmt.Println(*mountTag.Key, *mountTag.Value)
-										createVol(svc, *snapshot.SnapshotId, *definedTags.Value, *mountTag.Value, environment)
+										// fmt.Println()
+										// fmt.Printf("getSnapshot function %v: %v \n", *tag.Key, *tag.Value)
+										// fmt.Printf("getSnapshot function %v: %v === %v \n", *definedTags.Key, *definedTags.Value, broker)
+										// fmt.Println(*mountTag.Key, *mountTag.Value)
+										createVolID := createVol(svc, *snapshot.SnapshotId, *definedTags.Value, *mountTag.Value, environment, az)
+										time.Sleep(10 * time.Second)
+										attachVolume(svc, createVolID, *mountTag.Value, environment)
 									}
 								}
 							}
@@ -65,12 +73,12 @@ func getSnapshot(svc *ec2.EC2, environment string, brokers, mountPoints []string
 	}
 }
 
-func createVol(svc *ec2.EC2, snapshotID, broker, mountTag, env string) (string, string) {
+func createVol(svc *ec2.EC2, snapshotID, broker, mountTag, env, az string) string {
 	volTagName := fmt.Sprintf("%v-%v-%v", broker, mountTag, snapshotID)
 
 	input := &ec2.CreateVolumeInput{
 		// need to find out how to dynamic AZ
-		AvailabilityZone: aws.String("us-east-1a"),
+		AvailabilityZone: aws.String(az),
 		SnapshotId:       aws.String(snapshotID),
 		VolumeType:       aws.String("gp2"),
 		TagSpecifications: []*ec2.TagSpecification{
@@ -111,9 +119,8 @@ func createVol(svc *ec2.EC2, snapshotID, broker, mountTag, env string) (string, 
 		log.Fatal(err)
 	}
 
-	fmt.Println(*result.SnapshotId)
-	fmt.Println(*result.VolumeId)
-	return *result.VolumeId, mountTag
+	// fmt.Printf("CreateVol function - %v: %v\n", *result.SnapshotId, *result.VolumeId)
+	return *result.VolumeId
 
 }
 
@@ -143,14 +150,15 @@ func getNewKafkaVolumes(svc *ec2.EC2, environment string) {
 	for _, reservation := range volResult.Reservations {
 		for _, instance := range reservation.Instances {
 			for _, blkdev := range instance.BlockDeviceMappings {
-				describeVols(svc, *blkdev.Ebs.VolumeId)
+				// fmt.Println("getNewKafkaVolumes Function", *blkdev.Ebs.VolumeId)
+				filterNewKafkaRootVol(svc, *blkdev.Ebs.VolumeId)
 			}
 		}
 	}
 }
 
 // filter out /dev/sda1
-func describeVols(svc *ec2.EC2, volID string) {
+func filterNewKafkaRootVol(svc *ec2.EC2, volID string) {
 	input := &ec2.DescribeVolumesInput{
 		Filters: []*ec2.Filter{
 			{
@@ -172,8 +180,8 @@ func describeVols(svc *ec2.EC2, volID string) {
 			if *attachment.Device == "/dev/sda1" {
 				continue
 			} else {
-				fmt.Printf("%v: %v\n", *attachment.VolumeId, *attachment.Device)
-				// detachVols(svc, *attachment.VolumeId)
+				// fmt.Printf("FilterNewKafkaRootVol Function - %v: %v\n", *attachment.VolumeId, *attachment.Device)
+				detachVols(svc, *attachment.VolumeId)
 			}
 		}
 	}
@@ -182,6 +190,7 @@ func describeVols(svc *ec2.EC2, volID string) {
 // detach new kafka volumes
 func detachVols(svc *ec2.EC2, volID string) {
 	detachResult, err := svc.DetachVolume(&ec2.DetachVolumeInput{
+		Force:    aws.Bool(true),
 		VolumeId: aws.String(volID),
 	})
 
@@ -189,7 +198,7 @@ func detachVols(svc *ec2.EC2, volID string) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(detachResult)
+	fmt.Println(*detachResult.State)
 
 }
 
@@ -229,7 +238,7 @@ func attachVolume(svc *ec2.EC2, VolID, mountTagID, environment string) {
 				log.Fatal(err)
 			}
 
-			fmt.Println(attachResult)
+			fmt.Println(*attachResult.State)
 		}
 	}
 
